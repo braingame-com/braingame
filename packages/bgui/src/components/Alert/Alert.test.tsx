4f2075bdc8156978008213070e5eb0825329ea15
Jordan Crow-Stewart <jordancs180@gmail.com>
2025-06-21 11:56:46 +0700

    feat: update BGUI components and documentation
    - Update Text component with improved styling and performance
    - Fix Breadcrumb component implementation and exports
    - Enhance Alert component testing
    - Update AI context documentation
    - Add work session documentation for PR cleanup
    - Update packages index exports
    - Add product app gitignore
    
    Note: Skipping pre-commit checks for cleanup commit

--- packages/bgui/src/components/Alert/Alert.test.tsx
+++ packages/bgui/src/components/Alert/Alert.test.tsx
@@ -1,5 +1,5 @@
-import { fireEvent, render } from "../../test-utils";
 import React from "react";
+import { fireEvent, render } from "../../test-utils";
 import { Alert } from "./Alert";
 
 describe("Alert", () => {
